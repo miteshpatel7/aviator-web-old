9b81bd2f5939f9e3344779e9f093eed7a3e627f2
dev-dhruvin <dhruvinsudani.7seasol@gmail.com>
2022-06-18 18:38:56 +0530

    header and registration model design

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Registration from "../../models/Registration";
 
 const Header: React.FC = () => {
 
+    const [registrationModed, setRegistrationModed] = useState(false)
     return (
         <>
-            <h1>Header</h1>
+            <div className="position-fixed header">
+                <Navbar>
+                    {/* <Container style={{justifyContent:"space-between"}}> */}
+                    <div className="d-flex justify-content-between w-100 header-content">
+                        <h2>Logo</h2>
+                        <Nav className="ms-auto nav_link">
+                            <Nav.Link href="#" className="active_tabs">Home</Nav.Link>
+                            <Nav.Link href="#" className="disable_tabs">Features</Nav.Link>
+                            <button className="log-in-btn">Log In</button>
+                            <button className="Registration" onClick={() => setRegistrationModed(!registrationModed)}>Registration</button>
+                        </Nav>
+                    </div>
+                    {/* </Container> */}
+                </Navbar>
+            </div>
+
+            {registrationModed && <Registration show={registrationModed} onHide={() => setRegistrationModed(false)} />}
         </>
     );
 };
---
+++ src/models/Registration.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Dropdown, DropdownButton, Form, InputGroup, Modal } from 'react-bootstrap';
+
+interface Props {
+    onHide: () => void;
+    show?: boolean;
+}
+
+const Registration: React.FC<Props> = ({ show, onHide }) => {
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            className="p-5 registration-model"
+        >
+            <Modal.Header closeButton className='registration-model-header'>
+                Registration
+            </Modal.Header>
+            <Modal.Body>
+                <div className="registration-model-body">
+                    <input type="text" placeholder='Indian Rupee' className='mt-3' />
+                    <InputGroup className="mt-3">
+                        <DropdownButton
+                            variant="outline-secondary"
+                            title="+91"
+                            id="input-group-dropdown-1"
+                        >
+                            <Dropdown.Item href="#">+93</Dropdown.Item>
+                            <Dropdown.Item href="#">+42</Dropdown.Item>
+                            <Dropdown.Item href="#">+99</Dropdown.Item>
+                            <Dropdown.Item href="#">+81</Dropdown.Item>
+                        </DropdownButton>
+                        <Form.Control aria-label="Text input with dropdown button" placeholder='9632588525' />
+                    </InputGroup>
+                    <input type="text" placeholder='E-mail' className='mt-3' />
+                    <input type="text" placeholder='Password' className='mt-3' />
+                    <div className='align-items-center mt-3 condition'>
+                        <input type="checkbox" placeholder='Password' className='chechbox_input' />
+                        <label htmlFor="" className='ms-2'>I agree with 1win website </label>
+                    </div>
+                    <button className='registra-btn'>Registration</button>
+                </div>
+            </Modal.Body>
+        </Modal>
+    )
+}
+
+export default Registration
