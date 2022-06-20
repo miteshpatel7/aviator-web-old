ea422fc3fd92fbb8295a526d68a9811c9b4cf6e6
piyush koladiya <piyushkoladiya.7seasol@gmail.com>
2022-06-20 18:23:47 +0530

    design AuthHerader

---
+++ src/components/Balancemodal.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Balancemodal = () => {
+    return (
+        <div className="tooltiptext">
+            <p>Balance</p>
+            <p>Tooltip text</p>
+            <p>Tooltip text</p>
+            <p>Tooltip text</p>
+        </div>
+    )
+}
+
+export default Balancemodal
--- src/layouts/header/AuthHeader.tsx
+++ src/layouts/header/AuthHeader.tsx
@@ -1,4 +1,5 @@
-import { Nav, Navbar } from "react-bootstrap";
+import { Button, Form, InputGroup, Nav, Navbar } from "react-bootstrap";
+// import Balanceicon from 'assets/img/Vector.svg'
 
 const AuthHeader: React.FC = () => {
 
@@ -11,16 +12,35 @@ const AuthHeader: React.FC = () => {
             <Nav className="ms-auto nav_link">
               <Nav.Link href="#" className="active_tabs">Home</Nav.Link>
               <Nav.Link href="#" className="disable_tabs">Aviator</Nav.Link>
-              <Nav.Link href="#" className="disable_tabs add-balance d-flex align-items-center gap-2">Balance <img src="./assets/img/Vector.svg" alt="" />
+              <Nav.Link href="#" className="disable_tabs add-balance d-flex align-items-center gap-2">Balance
+                <img src='./assets/img/Vector.svg' alt="" />
                 <p className="mb-0 ms-3 active_tabs">₹ 0.00 </p>
                 <span className="tooltiptext">
-                  <p>Balance</p>
-                  <p>Tooltip text</p>
-                  <p>Tooltip text</p>
-                  <p>Tooltip text</p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <p>Balance</p>
+                    <div className="add-balance-icon">
+                      <img src="./assets/img/pluse.png" alt="" />
+                    </div>
+                  </div>
+                  <InputGroup size="lg" className="mt-4">
+                    <InputGroup.Text id="inputGroup-sizing-lg"><img src="./assets/img/rupya.png" alt="" /></InputGroup.Text>
+                    <Form.Control
+                      aria-label="Large"
+                      aria-describedby="inputGroup-sizing-sm"
+                    />
+                  </InputGroup>
+                  <Button className="wallet-management">Wallet Management</Button>
                 </span>
               </Nav.Link>
-              <button className="user-btn"><img src="./assets/img/User.png" alt="" /></button>
+              <button className="user-btn add-balance"><img src="./assets/img/User.png" alt="" />
+                <span className="tooltiptext" style={{width:"200px", textAlign:"right"}}>
+                  <h5>Withdrawal</h5>
+                  <h5>Transfer</h5>
+                  <h5>Bets History</h5>
+                  <h5>Settings</h5>
+                  <h5>Log Out</h5>
+                </span>
+              </button>
               <button className="Registration" onClick={() => { }}>Deposit</button>
             </Nav>
           </div>
